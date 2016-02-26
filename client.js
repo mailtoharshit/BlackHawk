7dfbd24be6bdeead9059c572a9e77f001db3e7ee
Harshit Pandey <mailtoharshit@gmail.com>
2016-02-25 23:40:51 -0800

    Update client.js

--- client.js
+++ client.js
@@ -1,35 +1,8 @@
 /**
- * Created with JetBrains WebStorm.
- * User: rohitghatol
- * Date: 6/28/13
- * Time: 5:47 PM
+ * Created with  WebStorm
  * To change this template use File | Settings | File Templates.
  */
 var arDrone = require('ar-drone');
-
-
-
-
-//client.createRepl();
-//client.animateLeds('blinkRed', 5, 10);
-
-//client.on('navdata', console.log);
-
-//client.takeoff();
-//client
-//    .after(5000, function() {
-//
-//        this.clockwise(0.5);
-//        client.animateLeds('blinkRed', 5, 2);
-//    })
-//    .after(3000, function() {
-//        //this.animate('flipLeft', 15);
-//    })
-//    .after(1000, function() {
-//        this.stop();
-//        this.land();
-//    });
-
 var io = require('socket.io-client'),
     socket = io.connect('localhost', {
         port: 3111
